3481bb114217fcbe840978d4bc5710153578c114
HarkHorning <hjhorning@gmai.com>
2025-07-07 20:51:06 -0400

    Trying something.

---
+++ src/main/java/hark/ecom/controllers/DevController.kt
@@ -0,0 +1,20 @@
+//package hark.ecom.controllers
+//
+//import org.springframework.web.bind.annotation.*
+//
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
+//
+//@RestController
+//@RequestMapping("/api/dev")
+//class DevController {
+//
+//    @GetMapping("/kotlin")
+//    fun sayHello(): String {
+//        return "Hello from Spring Boot Kotlin Controller!"
+//    }
+//
+//}
+//
+//data class Message(val content: String)
