cd93423dd92d9738d01364460cfb462f31cf2b78
HarkHorning <hjhorning@gmai.com>
2025-07-18 00:07:44 -0400

    Small changes.

--- src/main/java/hark/ecom/controllers/CustomerController.java
+++ src/main/java/hark/ecom/controllers/CustomerController.java
@@ -2,10 +2,10 @@ package hark.ecom.controllers;
 
 import hark.ecom.entities.Customer;
 import hark.ecom.repositories.CustomerRepository;
+import hark.ecom.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -15,16 +15,25 @@ import java.util.Optional;
 public class CustomerController {
 
     private CustomerRepository customerRepository;
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerRepository customerRepository, CustomerService customerService) {
+        this.customerRepository = customerRepository;
+        this.customerService = customerService;
+    }
 
     public CustomerController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
+    @PostMapping("/signup")
+    public Customer newCustomer(@RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
     @RequestMapping("/self")
-    public Customer getSelf(@Param("id") long id) {
-
-        Optional<Customer> self = customerRepository.findById(id);
-
-        return self.orElse(null);
+    public Customer findCustomerById(@Param("id") long id) {
+        return customerService.findCustomerById(id);
     }
 }
---
+++ src/main/java/hark/ecom/services/CustomerService.java
@@ -0,0 +1,29 @@
+package hark.ecom.services;
+
+import hark.ecom.entities.Customer;
+import hark.ecom.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer findCustomerById(long id) {
+
+        Optional<Customer> self = customerRepository.findById(id);
+        return self.orElse(null);
+    }
+}
