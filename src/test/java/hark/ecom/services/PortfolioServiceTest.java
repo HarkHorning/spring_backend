87331bd190eb1dcd83bed1c45d80c9104fb338f6
HarkHorning <hjhorning@gmai.com>
2025-10-27 15:54:44 -0600

    Added tests for Portfolio Service. Part D.

---
+++ src/test/java/hark/ecom/services/PortfolioServiceTest.java
@@ -0,0 +1,36 @@
+package hark.ecom.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PortfolioServiceTest {
+
+    @Test
+    void createPortfolio() {
+    }
+
+    @Test
+    void updatePortfolio() {
+    }
+
+    @Test
+    void getPortfolios() {
+    }
+
+    @Test
+    void getPortfolio() {
+    }
+
+    @Test
+    void getProductsByPortfolio() {
+    }
+
+    @Test
+    void addProductToPortfolio() {
+    }
+
+    @Test
+    void deletePortfolio() {
+    }
+}
